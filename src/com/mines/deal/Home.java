a4fb168b362c0810c381ca1217c00084e8bae44a
Thomas George <tfjgeorge@gmail.com>
2012-03-07 19:44:37 +0100

    home layout

--- src/com/mines/deal/Home.java
+++ src/com/mines/deal/Home.java
@@ -1,7 +1,11 @@
 package com.mines.deal;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
 
 public class Home extends Activity {
     /** Called when the activity is first created. */
